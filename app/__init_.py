5bb950845e003dbb48194c0aae81eb41482bc8fb
ilanabdulla <ilanabdulla18@gmail.com>
2026-09-08 09:56:00 +0530

    Update __init_.py

--- app/__init_.py
+++ app/__init_.py
@@ -1,1 +1,22 @@
+import os , urllib.parse , urllib.request , render_template
+
+gemini_api_key = "Gemini_API_Key";
+
+def home()
+ return render_template (" indexx.html;")
+
+def create_app():
+  app = Flask(_name_)
+  app.register_blueprint(youtube_bp, url_)prefix ="/youtube")
+
+@app.route("/html")
+def html():
+  return render_template("index.html")
+
+return app;
+
+
+
+
+
 
