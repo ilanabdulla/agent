c9ae330544fdb304964fc7d51627464cf2a155c8
Ilan <ilanabdulla18@gmail.com>
2026-09-10 14:24:20 +0530

    Update gmail_write.py

--- app/Gmail/gmail_write.py
+++ app/Gmail/gmail_write.py
@@ -7,4 +7,8 @@ KEYWORDS = (
     "write an email", "send an email", "draft an email",
     "compose an email", "write mail", "send mail", "draft mail",
     "compose mail"
-)
+ )    
+
+def is_email_command(text):
+    text = text.lower()
+    return  any(k in text for k in KEYWORDS)
