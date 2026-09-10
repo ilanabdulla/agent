69a63bc2416c6eb1ecfd6b5b3be86a4b1812bfe7
Ilan <ilanabdulla18@gmail.com>
2026-09-10 14:47:32 +0530

    Update gmail_write.py

--- app/Gmail/gmail_write.py
+++ app/Gmail/gmail_write.py
@@ -12,3 +12,27 @@ KEYWORDS = (
 def is_email_command(text):
     text = text.lower()
     return  any(k in text for k in KEYWORDS)
+
+def extract_email(text)
+    match = re.search(r"[\w.+-]+@[\w.-]+\.\w+", text)
+    if match:
+        return match.group(0)
+
+    match = re.search(
+        r"([\w..+-]+)\s+at\s+([\w.-]+)\s+dot\s+(\w+)",
+        text.lower()
+    )
+    if match:
+        return f"{match.group(1)}@{match.group(2)}.{match.group(3)"
+
+        return""
+
+def create_gmail_url(subject="",body="", recipient=""):
+    params = urllib.parse.urlencode({
+        "view" : "cm",
+        "fs" : "1",
+        "to" : recipent,
+        "su" : subject,
+        "body" : body
+    })
+    return f"https://mail.google.com/mail/u/0/?{params}"
